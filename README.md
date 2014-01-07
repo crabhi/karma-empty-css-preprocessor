0ac7da195271a8b9634ec3957b1444541cd5d11a
Filip Sedlak <sedlak@flih.cz>
2014-01-07 15:18:42 +0100

    Switch to empty-css from the coffee preprocessor.

--- README.md
+++ README.md
@@ -1,56 +1,26 @@
-# karma-coffee-preprocessor
+# karma-empty-css-preprocessor
 
-> Preprocessor to compile CoffeeScript on the fly.
+> Preprocessor to compile whatever file to empty .css
+
+This plugin may be useful if in your unit tests you aren't interested in the css,
+but the requireJS will refuse to work if it can't find the files.
+
+It is faster to create empty files than to run the usual machinery of SASS or LESS.
 
 ## Installation
 
-**This plugin ships with Karma by default, so you don't need to install it, it should just work ;-)**
-
-The easiest way is to keep `karma-coffee-preprocessor` as a devDependency in your `package.json`.
+The easiest way is to keep `karma-empty-css-preprocessor` as a devDependency in your `package.json`.
 ```json
 {
   "devDependencies": {
     "karma": "~0.10",
-    "karma-coffee-preprocessor": "~0.1"
+    "karma-empty-css-preprocessor": "~0.1"
   }
 }
 ```
 
 You can simple do it by:
 ```bash
-npm install karma-coffee-preprocessor --save-dev
+npm install karma-empty-css-preprocessor --save-dev
 ```
 
-## Configuration
-Following code shows the default configuration...
-```js
-// karma.conf.js
-module.exports = function(config) {
-  config.set({
-    preprocessors: {
-      '**/*.coffee': ['coffee']
-    },
-
-    coffeePreprocessor: {
-      // options passed to the coffee compiler
-      options: {
-        bare: true,
-        sourceMap: false
-      },
-      // transforming the filenames
-      transformPath: function(path) {
-        return path.replace(/\.js$/, '.coffee');
-      }
-    }
-  });
-};
-```
-
-If you set the `sourceMap` coffee compiler option to `true` then the generated source map will be inlined as a data-uri.
-
-----
-
-For more information on Karma see the [homepage].
-
-
-[homepage]: http://karma-runner.github.com
--- index.js
+++ index.js
@@ -1,54 +1,19 @@
-var coffee = require('coffee-script');
 var path = require('path');
-var createCoffeePreprocessor = function(args, config, logger, helper) {
-  config = config || {};
-
-  var log = logger.create('preprocessor.coffee');
-  var defaultOptions = {
-    bare: true,
-    sourceMap: false
-  };
-  var options = helper.merge(defaultOptions, args.options || {}, config.options || {});
-
-  var transformPath = args.transformPath || config.transformPath || function(filepath) {
-    return filepath.replace(/\.coffee$/, '.js');
+var createCssPreprocessor = function(args) {
+  var transformPath = function(filepath) {
+    return filepath.replace(/\..+$/, '.css');
   };
 
   return function(content, file, done) {
-    var result = null;
-    var map;
-    var datauri;
-
-    log.debug('Processing "%s".', file.originalPath);
     file.path = transformPath(file.originalPath);
 
-    // Clone the options because coffee.compile mutates them
-    var opts = helper._.clone(options)
-
-    try {
-      result = coffee.compile(content, opts);
-    } catch (e) {
-      log.error('%s\n  at %s:%d', e.message, file.originalPath, e.location.first_line);
-      return;
-    }
-
-    if (result.v3SourceMap) {
-      map = JSON.parse(result.v3SourceMap)
-      map.sources[0] = path.basename(file.originalPath)
-      map.sourcesContent = [content]
-      map.file = path.basename(file.path)
-      file.sourceMap = map;
-      datauri = 'data:application/json;charset=utf-8;base64,' + new Buffer(JSON.stringify(map)).toString('base64')
-      done(result.js + '\n//@ sourceMappingURL=' + datauri + '\n');
-    } else {
-      done(result.js || result)
-    }
+    done("");
   };
 };
 
-createCoffeePreprocessor.$inject = ['args', 'config.coffeePreprocessor', 'logger', 'helper'];
+createCssPreprocessor.$inject = ['args'];
 
 // PUBLISH DI MODULE
 module.exports = {
-  'preprocessor:coffee': ['factory', createCoffeePreprocessor]
+  'preprocessor:empty-css': ['factory', createCssPreprocessor]
 };
